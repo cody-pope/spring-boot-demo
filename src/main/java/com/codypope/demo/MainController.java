6f9e73cf8c8585dca3bfba6e6190de8a0060c8d2
Cody Pope <cody@redhat.com>
2021-08-10 10:51:23 -0400

    add property value

--- src/main/java/com/codypope/demo/MainController.java
+++ src/main/java/com/codypope/demo/MainController.java
@@ -1,14 +1,18 @@
 package com.codypope.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @Value("${greeting}")
+    private String greeting;
+
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return greeting;
     }
 
 }
